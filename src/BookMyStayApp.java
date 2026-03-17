f46ea6c11ddd2194b121744ac71e2f2dad65e580
kapil903 <bikhil098@gmail.com>
2026-03-17 18:58:39 +0530

    Initial commit: Hotel Booking System project setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,9 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("        Book My Stay App");
+        System.out.println("    Hotel Booking System v1.0");
+        System.out.println("=================================");
+        System.out.println("Welcome to the Hotel Booking Management System");
+    }
+}
